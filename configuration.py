4bf9991ebe3fa006d4b4acb204727e9c1c4dae4c
имя_пользователяTatianaKvasova <trondinatatiana@gmail.com>
2025-01-08 12:52:59 +0300

    изменения

--- configuration.py
+++ configuration.py
@@ -1,1 +1,6 @@
 #Татьяна Квасова, 25-я когорта, дипломный проект Яндекс Самокат
+URL_SERVICE = "https://1bc3597c-36fd-4c6e-a877-f623aebf7a9f.serverhub.praktikum-services.ru"
+
+CREATE_ORDER = "/api/v1/orders"
+
+PUT_ORDER = "/api/v1/orders/track"
